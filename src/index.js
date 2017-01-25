695e06b48bc32dc1461e889ac52f5868d99348fc
Rishi Karri <rishikarri1@gmail.com>
2017-01-25 15:41:10 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// bring in styles that are created with sass
+import './styles.css'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
