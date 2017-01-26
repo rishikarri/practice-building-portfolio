7b156480aa9b84d2508cd18bf4eeb6e04a75fc29
Rishi Karri <rishikarri1@gmail.com>
2017-01-25 19:05:04 -0500

    styling edits

--- src/Components/Hi.js
+++ src/Components/Hi.js
@@ -6,9 +6,9 @@ class Hi extends Component{
 
 	render(){
 		return(
-			<div className="full-view-height">
+			<div>
 
-				<div className="mid-and-center"> 
+				<div className="mid-and-center beast-text"> 
 					<div>Rishi Karri </div>
 					<div>Full Stack Web Developer </div> 									
 				</div>				
