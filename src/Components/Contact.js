d7d4e68f2f850a1ac44aa9e52ff94fb6df108262
Rishi Karri <rishikarri1@gmail.com>
2017-01-30 18:43:01 -0500

    layout for contact section updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,8 +7,70 @@ class Contact extends Component{
 	render(){
 		return(
 			<div>
+				<div>
+					
+					<div className='quote-text'>
+						I like building things and meeting new people! 
+					</div>
 
-				<i className="fa fa-linkedin-square fa-5x" aria-hidden="true"></i>
+					<div className='quote-text'>
+						Learn more / Say Hi:
+					</div>					
+
+					<div className='col-xs-6 col-xs-offset-3 contact-me-section margin-above'>
+
+
+
+						<div className='give-me-some-space margin-above'>
+							<ul className='no-bullet'>
+								<li>
+									<i className="fa fa-envelope fa-5x" aria-hidden="true"></i>								
+								</li>
+								<li>
+									Email
+								</li>
+							</ul>						
+						</div>
+
+						<div className='give-me-some-space'>
+							<ul className='no-bullet'>
+								<li>
+									<i className="fa fa-github-square fa-5x" aria-hidden="true"></i>
+								</li>
+								<li>
+									Github
+								</li>
+							</ul>						
+						</div>						
+
+						<div className='give-me-some-space'>
+							<ul className='no-bullet'>
+								<li>
+									<i className="fa fa-file-pdf-o fa-5x" aria-hidden="true"></i>
+								</li>
+								<li>
+									Resume
+								</li>
+							</ul>						
+						</div>						
+
+
+						<div className='give-me-some-space'>
+							<ul className='no-bullet'>
+								<li>
+									<i className="fa fa-linkedin-square fa-5x" aria-hidden="true"></i>
+								</li>
+								<li>
+									LinkedIn
+								</li>
+							</ul>						
+						</div>																		
+
+					
+
+					</div>
+					
+				</div>
 
 			</div>
 		)
